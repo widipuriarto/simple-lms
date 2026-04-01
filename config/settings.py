f2e28d0310f2d443523e8c423758ea8c39e006d3
widipuriarto <widipuriarto@gmail.com>
2026-04-01 23:59:29 +0700

    add import os

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from pathlib import Path
 
